c3b0abb23c54d8343bcbeba67980f037fd2aeeea
throcha3 <throcha3@gmail.com>
2024-09-07 16:18:02 -0300

    create routes and basic pages files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
+    <div className='App'>
+        <nav id="navBar">
+          <h2>
+            <Link to="/">Movies List</Link>
+            </h2>
+            <Link to="/movie/1">Movie</Link>
+            <Link to="/search">Search</Link>
+        </nav>
         <h2>Movies List</h2>
       </div>
-    </>
   )
 }
 
